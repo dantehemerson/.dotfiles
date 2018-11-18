d995188b26fc38af8957bc7336258efd5c04bc41
dantehemerson <dantehemerson@gmail.com>
2018-11-18 15:23:33 -0500

    Basic installation. Install git and curl

---
+++ installer.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "       ______      _    __ _ _                   "
+echo "       |  _  \    | |  / _(_) |                  "
+echo "       | | | |___ | |_| |_ _| | ___  ___         "
+echo "       | | | / _ \| __|  _| | |/ _ \/ __|        "
+echo "       | |/ / (_) | |_| | | | |  __/\__ \        "
+echo "       |___/ \___/ \__|_| |_|_|\___||___/        "                                  
+echo "                                                 "
+echo "              ❤ Dante's dotfiles ❤              "
+echo "                 @dantehemerson                  "
+echo ""
+
+echo 'Updating repositories ...'
+sudo apt-get update -y && sudo apt-get upgrade
+
+echo "Installing git ..."
+sudo apt-get install git -y
+
+echo "Installing curl ..."
+sudo apt-get install curl -y
