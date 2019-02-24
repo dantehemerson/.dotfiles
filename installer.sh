d6a4535f750522694ba453c420b1a957b352c7bf
Dante Calderón <dantehemerson@gmail.com>
2019-02-24 12:03:47 -0500

    Remove latex installation

--- installer.sh
+++ installer.sh
@@ -36,7 +36,7 @@ echo "Installing Ruby ..."
 sudo apt-get install ruby -y
 sudo apt-get install ruby-dev -y
 
-echo "Installing LaTex, This will take a long time ..."
-sudo apt-get install texlive-full -y
-echo "Installing Texmaker, This will take a long time ..."
-sudo apt-get install texmaker -y
+# echo "Installing LaTex, This will take a long time ..."
+# sudo apt-get install texlive-full -y
+# echo "Installing Texmaker, This will take a long time ..."
+# sudo apt-get install texmaker -y
