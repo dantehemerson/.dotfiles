ea28efc1e94bebdb33ddb2ae93f1d3ee1b76e22f
Dante Calderón <dantehemerson@gmail.com>
2019-03-03 11:03:29 -0500

    Add aliases

---
+++ zsh/.zsh/alias.zsh
@@ -0,0 +1,13 @@
+alias up="cd ../"
+alias v="vim"
+alias g="git"
+alias ga="git add"
+alias gc="git commit -m"
+alias gs="git status"
+alias gd="git diff"
+alias gf="git fetch"
+alias gm="git merge"
+alias gr="git rebase"
+alias gp="git push"
+alias gu="git unstage"
+alias disks='echo "╓───── m o u n t . p o i n t s"; echo "╙────────────────────────────────────── ─ ─ "; lsblk -a; echo ""; echo "╓───── d i s k . u s a g e"; echo "╙────────────────────────────────────── ─ ─ "; df -h;'
