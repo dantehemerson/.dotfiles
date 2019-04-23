6b264a3698d7c34910557ed609729344a92e99ac
Dante Calderón <dantehemerson@gmail.com>
2019-04-23 15:28:16 -0500

    Update alias.zsh

--- zsh/.zsh/alias.zsh
+++ zsh/.zsh/alias.zsh
@@ -11,3 +11,4 @@ alias gr="git rebase"
 alias gp="git push"
 alias gu="git unstage"
 alias disks='echo "╓───── m o u n t . p o i n t s"; echo "╙────────────────────────────────────── ─ ─ "; lsblk -a; echo ""; echo "╓───── d i s k . u s a g e"; echo "╙────────────────────────────────────── ─ ─ "; df -h;'
+alias t="npm t" # Execute test in nodejs project
