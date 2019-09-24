1a143f3324ac6d855a476f24a2fc97a3addabd40
Dante Calderón <mail@dantecalderon.dev>
2019-09-24 17:45:08 -0500

    Update ui.sh

--- installers/ui.sh
+++ installers/ui.sh
@@ -2,3 +2,11 @@
 
 echo "Installing beautiful cursor theme..."
 sudo apt-get install breeze-cursor-theme -y
+
+
+echo "Installing paper icons theme..."
+sudo add-apt-repository -u ppa:snwh/ppa -y
+sudo apt install paper-icon-theme -y
+
+echo "Installing Arc Theme..."
+sudo apt-get install arc-theme
