d9628b871e7c56984204da79021e9b40f4d70d49
Dante Calderón <mail@dantecalderon.dev>
2020-01-25 16:05:45 -0500

    Update albert.sh

--- installers/albert.sh
+++ installers/albert.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_18.04/ /' > /etc/apt/sources.list.d/home:manuelschneid3r.list"
-wget -nv https://download.opensuse.org/repositories/home:manuelschneid3r/xUbuntu_18.04/Release.key -O Release.key
+sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_19.10/ /' > /etc/apt/sources.list.d/home:manuelschneid3r.list"
+wget -nv https://download.opensuse.org/repositories/home:manuelschneid3r/xUbuntu_19.10/Release.key -O Release.key
 sudo apt-key add - < Release.key
-sudo apt-get update -y
-sudo apt-get install albert -y
+sudo apt-get update
+sudo apt-get install albert
