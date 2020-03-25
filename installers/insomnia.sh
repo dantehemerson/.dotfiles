8264d488fcaf5b74e180a93ce92c3be29753edbb
Dante Calderon <mail@dantecalderon.dev>
2020-03-25 00:32:34 -0500

    Add insmonia installer

---
+++ installers/insomnia.sh
@@ -0,0 +1,8 @@
+echo "deb https://dl.bintray.com/getinsomnia/Insomnia /" \
+    | sudo tee -a /etc/apt/sources.list.d/insomnia.list
+
+wget --quiet -O - https://insomnia.rest/keys/debian-public.key.asc \
+    | sudo apt-key add -
+
+sudo apt-get update -y
+sudo apt-get install insomnia -y
