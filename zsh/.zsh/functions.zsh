bb9354f9fa9fd35991293a625074726855df334d
Dante Calderón <mail@dantecalderon.dev>
2020-03-25 10:35:15 -0500

    Update functions.zsh

--- zsh/.zsh/functions.zsh
+++ zsh/.zsh/functions.zsh
@@ -1,3 +1,18 @@
-kp() {
+# Kill port, eg. kp 8000
+function kp() {
   fuser -k "$1"/tcp
+}
+
+# Determine size of a file or total size of a directory
+function fs() {
+	if du -b /dev/null > /dev/null 2>&1; then
+		local arg=-sbh;
+	else
+		local arg=-sh;
+	fi
+	if [[ -n "$@" ]]; then
+		du $arg -- "$@";
+	else
+		du $arg .[^.]* ./*;
+	fi;
 }
