e9a729196beab5c9b0ad8ca689f388a1adba6a1a
Dante Calderón <mail@dantecalderon.dev>
2020-04-30 01:26:54 -0500

    Update alias.zsh

--- zsh/.zsh/alias.zsh
+++ zsh/.zsh/alias.zsh
@@ -10,15 +10,13 @@ alias gm="git merge"
 alias gr="git rebase"
 alias gu="git unstage"
 
-## Migrate to
+## Pull
 alias gl="git pull"
 alias glb="git pull origin \$(git rev-parse --abbrev-ref HEAD)"
 
 # Push current branch
 alias gp="git push"
 alias gpb="git push origin \$(git rev-parse --abbrev-ref HEAD)"
-
-alias disks='echo "╓───── m o u n t . p o i n t s"; echo "╙────────────────────────────────────── ─ ─ "; lsblk -a; echo ""; echo "╓───── d i s k . u s a g e"; echo "╙────────────────────────────────────── ─ ─ "; df -h;'
 
 # Copy default ssh
 alias cpssh="xclip -sel clip < ~/.ssh/id_rsa.pub"
