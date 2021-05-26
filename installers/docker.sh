048f9ca495eadbb274b4e1b7d89b97aa7a4e5e46
Dante Calderon <mail@dantecalderon.dev>
2021-05-25 21:00:06 -0500

    Add docker and docker-compose installer

---
+++ installers/docker.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "🔷 Instaling Docker"
+
+# 1. Update the apt package index and install packages to allow apt to use a repository over HTTPS:
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+# 2. Add Docker’s official GPG key:
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg -y
+
+# 3. Use the following command to set up the stable repository.
+sudo echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# 4. Install Docker Engine
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+
+echo "🔹 Docker installed correctly"
