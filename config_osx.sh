b4a0821c5cf098117b26d7f94c5c904ce29386d4
Dante Calderon <18385321+dantehemerson@users.noreply.github.com>
2023-04-09 01:03:58 -0500

    Add config

---
+++ config_osx.sh
@@ -0,0 +1,12 @@
+# Close any open System Preferences panes, to prevent them from overriding
+# settings we’re about to change
+osascript -e 'tell application "System Preferences" to quit'
+
+# ================ FINDER ===============
+
+# Finder: allow quitting via ⌘ + Q; doing so will also hide desktop icons
+defaults write com.apple.finder QuitMenuItem -bool true
+
+# Finder: show all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
