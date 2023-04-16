56c7327fdcd2633a5c4462244a53de718bd75327
Dante Calderon <18385321+dantehemerson@users.noreply.github.com>
2023-04-16 00:06:32 -0500

    Simplify linking

--- install_osx.sh
+++ install_osx.sh
@@ -1,10 +1,34 @@
-# == Link shell config
-# ZSH
-ln -sf "$PWD/.shell" "$HOME"
+# Move a file or folder to trash.
+# Used to moved files that already exists before create the symlink,
+# just in case we need to restore them.
+function move_to_trash() {
+  if [ -e "$1" ]; then
+    filename=$(basename "$1")
+    echo "Moving $filename to trash"
+    new_filename="$filename-$(date +%s)"
 
-mv -f "$HOME/.zshrc" "$HOME/.zshrc.backup"
-ln -sf "$PWD/.shell/.zshrc" "$HOME/.zshrc"
+    mv -f "$1" ~/.Trash/"$new_filename"
+  fi
+}
+
+function link() {
+  # Only link if file is not already linked
+  if [ ! -L "$2" ] && [ ! -d "$2" ]; then
+    move_to_trash "$2"
+    ln -sf "$1" "$2"
+  else
+    echo "File $2 is already linked"
+  fi
+}
+
+## =========== LINK FILES ===========
+
+# ZSH
+link "$PWD/.shell" "$HOME"
+link "$PWD/.shell/.zshrc" "$HOME/.zshrc"
 
 # Git Config
-mv -f "$HOME/.gitconfig" "$HOME/.gitconfig.backup"
-ln -sf "$PWD/user/.gitconfig" "$HOME/.gitconfig"
+link "$PWD/user/.gitconfig" "$HOME/.gitconfig"
+
+# Vimrc
+link "$PWD/user/.vimrc" "$HOME/.vimrc"
