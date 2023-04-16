b03159904083720b6017d5578bdac631d9365de0
Dante Calderon <18385321+dantehemerson@users.noreply.github.com>
2023-04-16 00:11:21 -0500

    Logs

--- install_osx.sh
+++ install_osx.sh
@@ -4,10 +4,10 @@
 function move_to_trash() {
   if [ -e "$1" ]; then
     filename=$(basename "$1")
-    echo "Moving $filename to trash"
     new_filename="$filename-$(date +%s)"
 
     mv -f "$1" ~/.Trash/"$new_filename"
+    echo "$filename moved to trash, you can restore it from there if needed"
   fi
 }
 
@@ -16,15 +16,16 @@ function link() {
   if [ ! -L "$2" ] && [ ! -d "$2" ]; then
     move_to_trash "$2"
     ln -sf "$1" "$2"
+    echo "🔗 File $2 linked"
   else
-    echo "File $2 is already linked"
+    echo "🔗 File $2 is already linked"
   fi
 }
 
 ## =========== LINK FILES ===========
 
 # ZSH
-link "$PWD/.shell" "$HOME"
+link "$PWD/.shell" "$HOME/.shell"
 link "$PWD/.shell/.zshrc" "$HOME/.zshrc"
 
 # Git Config
