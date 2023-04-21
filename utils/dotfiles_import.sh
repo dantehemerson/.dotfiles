d784c56010612d7cd6db4db34d9c75d673d32809
Dante Calderon <18385321+dantehemerson@users.noreply.github.com>
2023-04-21 10:23:37 -0500

    Update

--- utils/dotfiles_import.sh
+++ utils/dotfiles_import.sh
@@ -9,7 +9,8 @@ if [[ "$(uname)" == "Darwin" ]]; then
   # Terminal preferences
   cp ~/.dotfiles/macos/preferences/com.apple.Terminal.plist ~/Library/Preferences/
   plutil -convert binary1 ~/Library/Preferences/com.apple.Terminal.plist
-  # killall Terminal
+
+  echo "ℹ️ Terminal preferences updated, please restart Terminal.app to apply changes"
 fi
 
 echo "============== IMPORTING DOTFILES COMPLETE =============="
