0d91c2af2414446e5df21bd64255d6db04f81e96
Dante Calderon <18385321+dantehemerson@users.noreply.github.com>
2023-04-21 10:28:37 -0500

    Update

--- utils/dotfiles_backup.sh
+++ utils/dotfiles_backup.sh
@@ -8,6 +8,11 @@ if [[ "$(uname)" == "Darwin" ]]; then
   # Terminal preferences
   cp ~/Library/Preferences/com.apple.Terminal.plist ~/.dotfiles/macos/preferences/
   plutil -convert xml1 ~/.dotfiles/macos/preferences/com.apple.Terminal.plist
+
+  # Remove not important keys
+  plutil -remove 'NSWindow Frame TTWindow' ./macos/preferences/com.apple.Terminal.plist
+
+
 fi
 
 echo "============== BACKING UP DOTFILES COMPLETE ============="
