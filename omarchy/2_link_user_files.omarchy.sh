8011a39ff4dc7c9a829696b5afc690577fb0d6d0
Dante Calderon <18385321+dantehemerson@users.noreply.github.com>
2026-01-07 01:47:51 -0500

    Some config

---
+++ omarchy/2_link_user_files.omarchy.sh
@@ -0,0 +1,10 @@
+source ~/.dotfiles/utils/utils.sh
+
+## =========== link files ===========
+
+
+# Tmux
+link "$PWD/user/.tmux.conf" "$HOME/.tmux.conf"
+
+# Bash
+link "$PWD/omarchy/.bashrc" "$HOME/.bashrc"
