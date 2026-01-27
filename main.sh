352b34a8817208f78c5d451d95950792ca798606
Dante Calderon <18385321+dantehemerson@users.noreply.github.com>
2026-01-27 01:29:52 -0500

    feat: [DIGI-0] Update

--- main.sh
+++ main.sh
@@ -41,7 +41,7 @@ echo "PM: $CURRENT_PM"
 echo "DISTRO: $CURRENT_DISTRO"
 
 if [[ "$CURRENT_DISTRO" == "arch" ]]; then
-  ./install/arch.sh
+  ~/.dotfiles/install/arch.sh
   # ./linux/1_install_apps.linux.sh
   # ./linux/2_link_user_files.linux.sh
 
@@ -49,13 +49,4 @@ elif [[ "$CURRENT_DISTRO" == "macos" ]]; then
 
   ./macos/1_install_apps.osx.sh
   ./macos/2_configure.osx.sh
-fi
-
-# Install starship
-install_from_curl "https://starship.rs/install.sh" "starship"
-
-# Cleanup if we used temp dir for piped install
-if [[ ! -t 0 ]]; then
-  cd ~
-  rm -rf "$TEMP_DIR"
 fi
