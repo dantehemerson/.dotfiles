e266d543ad35e1aee11fbfe527c1af4114dd64e6
Dante Calderon <18385321+dantehemerson@users.noreply.github.com>
2026-01-28 01:51:46 -0500

    feat: [DIGI-0] Update

--- test/tests/macos_tests.sh
+++ test/tests/macos_tests.sh
@@ -13,18 +13,3 @@ assert_command_exists "starship"
 xcode-select -p >/dev/null 2>&1
 
 assert_file_is_symlink "$HOME/.inputrc"
-
-if command -v brew >/dev/null 2>&1; then
-  brew doctor >/dev/null 2>&1
-  brew update >/dev/null 2>&1
-fi
-
-bash_path=""
-if [ "$(uname -m)" = "arm64" ]; then
-  bash_path="/opt/homebrew/bin/bash"
-else
-  bash_path="/usr/local/bin/bash"
-fi
-
-grep -Fxq "$bash_path" /etc/shells
-[ -x "$bash_path" ]
