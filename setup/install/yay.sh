7c67e5c0f41bccf51495d67b4c4b550e21e4c7af
Dante Calderon <18385321+dantehemerson@users.noreply.github.com>
2026-01-28 01:31:46 -0500

    feat: [DIGI-0] Update

--- setup/install/yay.sh
+++ setup/install/yay.sh
@@ -5,5 +5,5 @@ load_packages "$HOME/.dotfiles/setup/packages/arch.yay.packages" packages
 printf "=> Installing common yay packages:\n"
 printf "\t- %s\n" "${packages[@]}"
 for pkg_cmd in "${packages[@]}"; do
-  yay -Syu --noconfirm --needed --cleanafter --nodiffmenu --nocleanmenu $pkg_cmd
+  yay -Syu --noconfirm --needed --cleanafter $pkg_cmd
 done
