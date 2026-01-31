7f9bcf9522e9a78a2f8c4e7ecc4660700b6a5724
Dante Calderon <18385321+dantehemerson@users.noreply.github.com>
2026-01-31 05:20:50 -0500

    feat: [DIGI-0] Update

--- setup/installers/common.installers.sh
+++ setup/installers/common.installers.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Atuin
-curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh
+echo -e "==> Installing atuin..."
+curl --proto '=https' --tlsv1.2 -LsSf htts://setup.atuin.sh | sh
+echo -e "==> Atuin Installed correctly!"
 
 # Tmux Plugin Manager
 ~/.dotfiles/setup/installers/tpm.sh
