e8ec78293e228453a535063431d6b5443e4db5fa
Dante Calderon <dante.calderon@globant.com>
2026-02-10 10:04:40 -0500

    Fix homebrew load on M1

--- shell/envs.sh
+++ shell/envs.sh
@@ -1,3 +1,14 @@
 # Editor used by CLI
 export SUDO_EDITOR="$EDITOR"
 export BAT_THEME=ansi
+
+
+# Load hombrew bin
+if [[ $(uname -m) == "arm64" ]]; then # Apple Silicon
+  export PATH="/opt/homebrew/bin:$PATH"
+  export PATH="/opt/homebrew/sbin:$PATH"
+else
+  export PATH="/usr/local/bin:$PATH"
+  export PATH="/usr/local/sbin:$PATH"
+fi
+
