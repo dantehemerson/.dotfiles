3b67b093dcdb5809c311336f43e923ac304ae6f1
Dante Calderon <18385321+dantehemerson@users.noreply.github.com>
2026-08-16 22:07:34 -0500

    Update

--- user/.config/nvim2/lua/config/lsp.lua
+++ user/.config/nvim2/lua/config/lsp.lua
@@ -10,19 +10,19 @@ vim.api.nvim_create_autocmd("LspAttach", {
 
 		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts("Go to definition"))
 		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts("Go to declaration"))
-		vim.keymap.set("n", "gr", vim.lsp.buf.references, opts())
-		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts())
+		vim.keymap.set("n", "gr", vim.lsp.buf.references, opts("Go to references"))
+		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts("Go to implementation"))
 
-		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts())
-		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts())
-		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts())
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts("Show hover"))
+		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts("Rename symbol"))
+		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts("Code action"))
 
 		vim.keymap.set("n", "[d", function()
 			vim.diagnostic.jump({ count = -1 })
-		end, opts())
+		end, opts("Previous diagnostic"))
 
 		vim.keymap.set("n", "]d", function()
 			vim.diagnostic.jump({ count = 1 })
-		end, opts())
+		end, opts("Next diagnostic"))
 	end,
 })
