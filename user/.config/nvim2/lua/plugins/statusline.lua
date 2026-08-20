f275b9b0a541788d2b70722e202399b45e973324
Dante Calderon <18385321+dantehemerson@users.noreply.github.com>
2026-08-20 00:56:13 -0500

    update

--- user/.config/nvim2/lua/plugins/statusline.lua
+++ user/.config/nvim2/lua/plugins/statusline.lua
@@ -1,6 +1,12 @@
 return {
 	"nvim-lualine/lualine.nvim",
 	config = function()
+		local function hi(group, attr)
+			attr = attr or "fg"
+			local h = vim.api.nvim_get_hl(0, { name = group, link = false })
+			return string.format("#%06x", h[attr] or 0xffffff)
+		end
+
 		require("lualine").setup({
 			options = {
 				icons_enabled = true,
@@ -18,13 +24,16 @@ return {
 					"location",
 					{
 						function()
-							local icon = "󰉋 "
+							local icon = " "
 							local name = vim.uv.cwd()
 							name = name:match("([^/\\]+)[/\\]*$") or name
-							return icon .. " " .. name
+							return icon .. "" .. name
 						end,
 						cond = function()
 							return vim.o.columns > 85
+						end,
+						color = function()
+							return { fg = hi("Normal", "bg"), bg = hi("Error"), gui = "bold" }
 						end,
 					},
 				},
