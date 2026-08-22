b5dd893ac56e19f5863fb0c55f79e49e0bd5b472
Dante Calderon <18385321+dantehemerson@users.noreply.github.com>
2026-08-21 20:02:34 -0500

    add neotree

---
+++ user/.config/nvim2/lua/plugins/neo-tree.lua
@@ -0,0 +1,40 @@
+return {
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		cmd = "Neotree",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+		},
+		lazy = false,
+		opts = {
+			window = {
+				mappings = {
+					["l"] = "open",
+					["h"] = "close_node",
+					["<space>"] = "none",
+					["Y"] = {
+						function(state)
+							local node = state.tree:get_node()
+							local path = node:get_id()
+							vim.fn.setreg("+", path, "c")
+						end,
+						desc = "Copy Path to Clipboard",
+					},
+					["O"] = {
+						function(state)
+							require("lazy.util").open(state.tree:get_node().path, { system = true })
+						end,
+						desc = "Open with System Application",
+					},
+					["P"] = { "toggle_preview", config = { use_float = false } },
+				},
+			},
+		},
+
+		keys = {
+			{ "<leader>e", "<leader>fe", desc = "Explorer NeoTree (Root Dir)", remap = true },
+		},
+	},
+}
--- user/.config/nvim2/lua/plugins/yazi.lua
+++ user/.config/nvim2/lua/plugins/yazi.lua
@@ -1,3 +1,7 @@
+if true then
+	return {}
+end
+
 return {
 	"mikavilpas/yazi.nvim",
 	event = "VeryLazy",
