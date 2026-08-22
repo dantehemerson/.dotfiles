d075fbe2314ed6e758d4ad7d1214e6ca940f7a43
Dante Calderon <18385321+dantehemerson@users.noreply.github.com>
2026-08-21 23:10:16 -0500

    update

--- user/.config/nvim2/lua/plugins/colorscheme.lua
+++ user/.config/nvim2/lua/plugins/colorscheme.lua
@@ -1,26 +1,27 @@
 return {
-	{
-		"andrew-george/telescope-themes",
-		config = function()
-			require("telescope").setup({
-				extensions = {
-					themes = {
-						-- allow default/builtin schemes (don't ignore them)
-						--						ignore = false,
-					},
-				},
-			})
-
-			require("telescope").load_extension("themes")
-
-			vim.keymap.set(
-				"n",
-				"<leader>th",
-				":Telescope themes<CR>",
-				{ noremap = true, silent = true, desc = "Theme Switcher" }
-			)
-		end,
-	},
+	-- {
+	-- 	"andrew-george/telescope-themes",
+	-- 	cmd = "Fff",
+	-- 	config = function()
+	-- 		require("telescope").setup({
+	-- 			extensions = {
+	-- 				themes = {
+	-- 					-- allow default/builtin schemes (don't ignore them)
+	-- 					--						ignore = false,
+	-- 				},
+	-- 			},
+	-- 		})
+	--
+	-- 		require("telescope").load_extension("themes")
+	--
+	-- 		vim.keymap.set(
+	-- 			"n",
+	-- 			"<leader>th",
+	-- 			":Telescope themes<CR>",
+	-- 			{ noremap = true, silent = true, desc = "Theme Switcher" }
+	-- 		)
+	-- 	end,
+	-- },
 
 	-- Themes:
 	{ "Mofiqul/vscode.nvim" },
---
+++ user/.config/nvim2/lua/plugins/fff.lua
@@ -0,0 +1,84 @@
+return {
+	{
+		"dmtrKovalenko/fff",
+		build = function()
+			-- downloads a prebuilt binary or falls back to cargo build
+			require("fff.download").download_or_build_binary()
+		end,
+		opts = {
+			prompt = "  ",
+			layout = {
+				prompt_position = "top",
+			},
+		},
+		lazy = false, -- the plugin lazy-initialises itself
+		keys = {
+			{
+				"<leader>ff",
+				function()
+					require("fff").find_files()
+				end,
+				desc = "Find files",
+			},
+			{
+				"<leader>/",
+				function()
+					require("fff").live_grep()
+				end,
+				desc = "Live grep",
+			},
+			{
+				"<leader>fg",
+				function()
+					require("fff").live_grep()
+				end,
+				desc = "Live grep",
+			},
+			{
+				"<leader>fz",
+				function()
+					require("fff").live_grep({ grep = { modes = { "fuzzy", "plain" } } })
+				end,
+				desc = "Live fuzzy grep",
+			},
+			{
+				"<leader>fw",
+				function()
+					require("fff").live_grep_under_cursor()
+				end,
+				mode = { "n", "x" },
+				desc = "Search current word / selection",
+			},
+		},
+	},
+	{
+		"vinitkumar/fff-plus.nvim",
+		dependencies = { "dmtrKovalenko/fff" },
+		opts = {
+			legacy_commands = false,
+		},
+		keys = {
+			{
+				"<leader>,",
+				function()
+					require("fff_plus").buffers()
+					vim.schedule(function()
+						local state = require("fff_plus.pickers.buffers").state
+						-- Move down to select the previous buffer
+						if state and state.active then
+							state:move("down")
+						end
+					end)
+				end,
+				desc = "Switch Buffer",
+			},
+			{
+				"<leader>fc",
+				function()
+					require("fff_plus").colors()
+				end,
+				desc = "Colorscheme",
+			},
+		},
+	},
+}
