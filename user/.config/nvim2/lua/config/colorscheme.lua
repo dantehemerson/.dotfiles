9bf81360e1f59adc9cbd8a18b4fd12fe3ab9be1d
Dante Calderon <18385321+dantehemerson@users.noreply.github.com>
2026-08-21 23:14:12 -0500

    update

--- user/.config/nvim2/lua/config/colorscheme.lua
+++ user/.config/nvim2/lua/config/colorscheme.lua
@@ -3,10 +3,6 @@ local theme_file = vim.fn.stdpath("config") .. "/lua/current-theme.lua"
 -- Save whenever the colorscheme changes
 vim.api.nvim_create_autocmd("ColorScheme", {
 	callback = function()
-		vim.notify("ColorScheme event fired: " .. tostring(vim.g.colors_name))
-		local ok = vim.fn.writefile({ string.format("vim.cmd.colorscheme(%q)", vim.g.colors_name) }, theme_file)
-		if ok == -1 then
-			vim.notify("writefile failed", vim.log.levels.ERROR)
-		end
+		vim.fn.writefile({ string.format("vim.cmd.colorscheme(%q)", vim.g.colors_name) }, theme_file)
 	end,
 })
