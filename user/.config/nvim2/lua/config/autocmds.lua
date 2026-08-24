dc723fdb9aa16e3a5705f280717a156b4c7e6a42
Dante Calderon <18385321+dantehemerson@users.noreply.github.com>
2026-08-24 00:58:21 -0500

    update

--- user/.config/nvim2/lua/config/autocmds.lua
+++ user/.config/nvim2/lua/config/autocmds.lua
@@ -3,3 +3,11 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.hl.on_yank()
 	end,
 })
+
+vim.api.nvim_create_augroup("AutoFormatting", {})
+vim.api.nvim_create_autocmd("BufWritePre", {
+	group = "AutoFormatting",
+	callback = function()
+		vim.lsp.buf.format({ async = true })
+	end,
+})
