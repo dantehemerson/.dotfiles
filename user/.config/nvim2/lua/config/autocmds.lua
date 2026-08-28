bf69ba688dca5a6b9fbd3fd404d7701a7a3894a7
Dante Calderon <18385321+dantehemerson@users.noreply.github.com>
2026-08-27 23:25:10 -0500

    update

--- user/.config/nvim2/lua/config/autocmds.lua
+++ user/.config/nvim2/lua/config/autocmds.lua
@@ -1,31 +1,31 @@
 vim.api.nvim_create_autocmd("TextYankPost", {
-	callback = function()
-		vim.hl.on_yank()
+  callback = function()
+    vim.hl.on_yank()
 
-		if vim.v.event.operator == "y" then
-			vim.fn.setreg("+", vim.fn.getreg('"'))
-		end
-	end,
+    if vim.v.event.operator == "y" then
+      vim.fn.setreg("+", vim.fn.getreg('"'))
+    end
+  end,
 })
 
 vim.api.nvim_create_augroup("AutoFormatting", {})
 vim.api.nvim_create_autocmd("BufWritePre", {
-	group = "AutoFormatting",
-	callback = function()
-		vim.lsp.buf.format({ async = true })
-	end,
+  group = "AutoFormatting",
+  callback = function()
+    vim.lsp.buf.format({ async = true })
+  end,
 })
 
 -- show cursorline only in active window:
 vim.api.nvim_create_autocmd({ "WinEnter", "BufEnter" }, {
-	group = vim.api.nvim_create_augroup("active_cursorline", { clear = true }),
-	callback = function()
-		vim.opt_local.cursorline = true
-	end,
+  group = vim.api.nvim_create_augroup("active_cursorline", { clear = true }),
+  callback = function()
+    vim.opt_local.cursorline = true
+  end,
 })
 vim.api.nvim_create_autocmd({ "WinLeave", "BufLeave" }, {
-	group = "active_cursorline",
-	callback = function()
-		vim.opt_local.cursorline = false
-	end,
+  group = "active_cursorline",
+  callback = function()
+    vim.opt_local.cursorline = false
+  end,
 })
